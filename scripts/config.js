8693697f6176a3701881be4a7302728f57b53d15
Brice <brice@gizra.com>
2015-01-21 15:15:58 +0200

    Built client from commit 6e5fcff on branch tracking-page

--- scripts/config.js
+++ scripts/config.js
@@ -1,5 +1,5 @@
 angular.module('config', [])
 
-.constant('Config', {backend:'http://dev-productivity.pantheon.io',debugUiRouter:false})
+.constant('Config', {backend:'http://live-productivity.pantheon.io',debugUiRouter:false})
 
 ;
