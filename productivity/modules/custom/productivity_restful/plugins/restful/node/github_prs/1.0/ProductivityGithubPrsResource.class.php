129f05b08f025fc9bfd10a7ce9caa971c77023da
Itamar Shapira Bar-Lev <bitamar@gmail.com>
2015-05-06 12:42:45 +0300

    Fixes

--- productivity/modules/custom/productivity_restful/plugins/restful/node/github_prs/1.0/ProductivityGithubPrsResource.class.php
+++ productivity/modules/custom/productivity_restful/plugins/restful/node/github_prs/1.0/ProductivityGithubPrsResource.class.php
@@ -8,12 +8,12 @@
 class ProductivityGithubPrsResource extends \ProductivityEntityBaseNode {
 
   /**
-   * Overrides \RestfulEntityBaseNode::publicFieldsInfo().
+   * Overrides \ProductivityEntityBaseNode::publicFieldsInfo().
    */
   public function publicFieldsInfo() {
     $public_fields = parent::publicFieldsInfo();
 
-    $public_fields['issueId'] = array('property' => 'field_issue_id');
+    $public_fields['issue'] = array('property' => 'field_issue_id');
 
     return $public_fields;
   }
@@ -22,30 +22,32 @@ class ProductivityGithubPrsResource extends \ProductivityEntityBaseNode {
   /**
    * Overrides RestfulEntityBase::getQueryForList().
    */
-  public function getEntityFieldQuery() {
+  public function getQueryForList() {
     $request = $this->getRequest();
     // Validate parameters.
     foreach (array('day', 'month', 'year', 'project_id') as $required_field) {
-      if (empty($request[$required_field]) && !intval($request[$required_field])) {
-        throw new \RestfulBadRequestException(t('Invalid @field', array('@field' => $required_field)));
+      if (empty($request[$required_field])) {
+        throw new \RestfulBadRequestException(format_string('Missing required parameter @field.', array('@field' => $required_field)));
       }
+      if (intval($request[$required_field]) != $request[$required_field]) {
+        throw new \RestfulBadRequestException(format_string('Wrong value for @field, expected integer.', array('@field' => $required_field)));
+      }
+
+      // Make sure the integer values are cleaned.
       $request[$required_field] = intval($request[$required_field]);
     }
 
-    $query = parent::getEntityFieldQuery();
+    $query = parent::getQueryForList();
 
     // Filter day.
-    $start_timestamp =  $request['year'] . '-' . $request['month'] . '-' . $request['day'] . ' 00:00:00';
-    $end_timestamp = date('Y-m-d 00:00:00', strtotime('+1 day', strtotime($start_timestamp)));
-    $query->fieldCondition('field_work_date', 'value', $start_timestamp, '>=');
-    $query->fieldCondition('field_work_date', 'value', $end_timestamp, '<');
+    $this->setWorkDateTimeSpan($query, '+1 day');
 
     // Filter project.
     $query->fieldCondition('field_project', 'target_id', $request['project_id']);
 
     // Filter employee.
-    if (!$account = user_load_by_name(check_plain($request['employee']))) {
-      throw new \RestfulBadRequestException(t('Invalid username'));
+    if (!$account = user_load_by_name($request['employee'])) {
+      throw new \RestfulBadRequestException(format_string('Invalid employee username: @username.', array('@username' => $request['employee'])));
     }
     $query->fieldCondition('field_employee', 'target_id', $account->uid);
 
