65ebc21b4fb778656452fd9b9311b3c62a06480e
Brice Lenfant <brice@gizra.com>
2015-07-11 23:58:22 +0300

    Logout menu

--- productivity/themes/custom/bootstrap_subtheme/templates/page.tpl.php
+++ productivity/themes/custom/bootstrap_subtheme/templates/page.tpl.php
@@ -253,7 +253,7 @@
                 <li><a href="user-profile.html"><i class="fa fa-user"></i>Profile</a></li>
                 <li><a href="#"><i class="fa fa-cog"></i>Settings</a></li>
                 <li><a href="#"><i class="fa fa-envelope-o"></i>Messages</a></li>
-                <li><a href="#"><i class="fa fa-power-off"></i>Logout</a></li>
+                <li><a href="<?php print url('user/logout');?>"><i class="fa fa-power-off"></i>Logout</a></li>
               </ul>
             </li>
             <li class="hidden-xxs">
