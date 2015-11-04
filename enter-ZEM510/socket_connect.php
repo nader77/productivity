fd6f95db35a4ab84f6154d7d7f414219a434bc3a
Itamar Shapira Bar-Lev <itamar@gizra.com>
2015-11-04 10:29:24 +0200

    Updating clock ip address

--- enter-ZEM510/socket_connect.php
+++ enter-ZEM510/socket_connect.php
@@ -12,7 +12,7 @@ echo "Socket created \n";
  
 
 //Connect socket to remote server
-if(!socket_connect($sock , '10.0.0.201' , 4370))
+if(!socket_connect($sock , '192.168.1.201', 4370))
 {
     $errorcode = socket_last_error();
     $errormsg = socket_strerror($errorcode);
@@ -30,7 +30,7 @@ $message = "\x50\x50\x82\x7d\x08\x00\x00\x00\xe8\x03\x17\xfc\x00\x00\x00\x00";
 $message2 = "\x50\x50\x82\x7d\x08\x00\x00\x00\x4c\x04\x6d\xce\x45\x2d\x01\x00";
 
 // Get data
-// $message = "\x50\x50\x82\x7d\x10\x00\x00\x00\xe0\x05\x01\xcd\x45\x2d\x18\x00\x00\x00\x00\x00\xc0\xff\x00\x00";
+//$message = "\x50\x50\x82\x7d\x10\x00\x00\x00\xe0\x05\x01\xcd\x45\x2d\x18\x00\x00\x00\x00\x00\xc0\xff\x00\x00";
 
 //Send the message to the server
 if( ! socket_send ( $sock , $message , strlen($message) , 0))
@@ -41,7 +41,7 @@ if( ! socket_send ( $sock , $message , strlen($message) , 0))
     die("Could not send data: [$errorcode] $errormsg \n");
 }
  
-echo "Message send successfully \n";
+echo "Message sent successfully \n";
  
 $buf = "";
 //Now receive reply from server
@@ -54,6 +54,7 @@ if( socket_recv( $sock , $buf , 2045 , MSG_PEEK ) === FALSE)
 }
  
 //print the received message
+echo "Buffer:\n";
 var_dump($buf);
 
 //Send the message to the server
@@ -65,7 +66,7 @@ if( ! socket_send ( $sock , $message2 , strlen($message2) , 0))
     die("Could not send data: [$errorcode] $errormsg \n");
 }
  
-echo "Message send successfully \n";
+echo "Message sent successfully \n";
  
 $buf = "";
 //Now receive reply from server
@@ -76,6 +77,7 @@ if( socket_recv( $sock , $buf , 2045 , MSG_PEEK ) === FALSE)
      
     die("Could not receive data: [$errorcode] $errormsg \n");
 }
- 
+
 //print the received message
+echo "Buffer:\n";
 var_dump($buf);
