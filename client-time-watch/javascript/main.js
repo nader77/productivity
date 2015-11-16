ef577681e40875ec766c71d2e4d83aa1726c570d
YaronMiro <yaron@gizra.com>
2015-11-16 17:22:03 +0200

    more funcitonal mar

--- client-time-watch/javascript/main.js
+++ client-time-watch/javascript/main.js
@@ -3,6 +3,9 @@ $(document).ready(function() {
   // Init default values.
   var maxDigits = 4;
   var digitsCounter = 0;
+  var $resetButton = $('button.-reset');
+  var pinCode = '';
+  var validPinCode = '1234';
 
   // Mockup the connection led light indicator.
   setInterval( function(){
@@ -14,12 +17,17 @@ $(document).ready(function() {
   var digitClickHandler = function() {
     var self = this;
 
+    var digitValue = $(self).text();
+    $($('.code .pin').get(digitsCounter)).text(digitValue);
+    pinCode += digitValue;
+
     // Increment digit counter.
     digitsCounter++;
 
     // Enable reset button
-    $('.digit.-reset').prop('disabled', false);
+    $resetButton.prop('disabled', false);
 
+    // Add active class
     $(self).addClass('-active');
 
     setTimeout( function(){
@@ -30,14 +38,24 @@ $(document).ready(function() {
       if (digitsCounter != maxDigits) {
         return;
       }
-
-      // Disable all digits buttons it it's the 4th digit that was clicked.
-      $('button').each(function() {
-        $(this).prop('disabled', true);
-      });
-
+      serverResponse();
     }, 45);
   };
+
+  // Demo click handler for the "reset" button.
+  var resetClickHandler = function() {
+    var self = this;
+
+    $(self).addClass('-active');
+
+    $($('.code .pin').get(digitsCounter - 1)).text('');
+    digitsCounter--;
+
+    setTimeout( function(){
+      // Remove class.
+      $(self).removeClass('-active');
+    }, 45);
+  }
 
   // Digit click handler callback.
   $(".numbers-pad .digit").on("click", digitClickHandler);
@@ -46,5 +64,40 @@ $(document).ready(function() {
   $(".projects button.item").on("click", function() {
     $(this).toggleClass('-active');
   });
+
+  // Mockup for the server response
+  function serverResponse() {
+    var $icon = $('.icon.dynamic');
+    $icon.append('<i class="fa fa-circle-o-notch fa-spin"></i>');
+
+    setTimeout(function(){
+      // Clear inner content first.
+      $icon.html('');
+
+      // Enable all digits buttons.
+      $('button').each(function() {
+        $(this).prop('disabled', false);
+      });
+
+      // In case of success.
+      if (pinCode === validPinCode) {
+        $icon.addClass('-success');
+        $icon.append('<i class="fa fa-check success"></i>');
+      }
+      // In case of error.
+      else {
+        $icon.addClass('-error');
+        $icon.append('<i class="fa fa-times error"></i>');
+      }
+    }, 300)
+
+    // Disable all digits buttons it it's the 4th digit that was clicked.
+    $('button').each(function() {
+      $(this).prop('disabled', true);
+    });
+  }
+
+  // Reset button click handler callback.
+  $resetButton.on("click", resetClickHandler);
 
 });
