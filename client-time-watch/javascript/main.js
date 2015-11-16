17494ac6e49f7d77570f839776ba95bd63d8cf26
YaronMiro <yaron@gizra.com>
2015-11-16 12:44:48 +0200

    Adding style

--- client-time-watch/javascript/main.js
+++ client-time-watch/javascript/main.js
@@ -32,13 +32,22 @@ $(document).ready(function() {
       }
       // Disable all digits it it's the 4th digit that was clicked.
       // Strip class "active" from the breakpoints icons.
-      $(".numbers-pad .digit").each(function() {
+      $('button').each(function() {
         $(this).prop('disabled', true);
       });
 
     }, 45);
   };
 
+  // Demo click handler for the "digit" button.
+  var ProjectClickHandler = function() {
+    var self = this;
+    $(self).toggleClass('-active');
+  };
+
   // Digit click handler callback.
   $(".numbers-pad .digit").on("click", digitClickHandler);
+
+  // Digit click handler callback.
+  $(".projects button.item").on("click", ProjectClickHandler);
 });
