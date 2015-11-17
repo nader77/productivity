145f8d5cb8af6e5c01ba0c24846210d88ffacc43
YaronMiro <yaron@gizra.com>
2015-11-17 13:23:19 +0200

    clean code

--- client-time-watch/javascript/main.js
+++ client-time-watch/javascript/main.js
@@ -6,8 +6,9 @@ $(document).ready(function() {
   var validPinCode = '1234';
 
   // Elements
-  var $deleteButton = $('button#delete');
+  var $deleteButton = $('button.-delete');
   var $codePin = $('.code .pin');
+  var $dynamicIcon = $('.-dynamic-icon');
 
   // Dummy ping to server to check connectivity.
   CheckServerConnection(1700);
@@ -16,37 +17,45 @@ $(document).ready(function() {
   var digitClickHandler = function() {
     var self = this;
 
-    // Display the target "digit" value.
-    $($codePin.get(digitsCounter)).text($(self).text());
-
-    // Increment digit counter.
-    digitsCounter++;
-    console.log(digitsCounter);
-
     // Add class "-active" for UX.
-    toggoleButtonActivity(self)
+    toggleButtonActivity(self)
 
     // Enable the "delete" button.
     $deleteButton.prop('disabled', false);
 
+    // Display the target "digit" value.
+    $($codePin.get(digitsCounter)).text($(self).text());
+    digitsCounter++;
+
     // Dummy request to server.
     if (digitsCounter == maxDigits) {
       serverResponse();
-      return;
     }
   };
 
-  // Demo click handler for the "reset" button.
-  var deleteButtonkHandler = function() {
+  // Dummy "delete" button click.
+  var deleteButtonHandler = function() {
     var self = this;
 
+    if (digitsCounter == maxDigits) {
+      // Delete icon.
+      $dynamicIcon.html('');
+    }
+
     // Add class "-active" for UX.
-    toggoleButtonActivity(self)
+    toggleButtonActivity(self)
 
     // Delete the last digit from the record.
     $($codePin.get(digitsCounter - 1)).text('');
     digitsCounter--;
+
+    // In case we have no "digit" at all.
+    if (!digitsCounter) {
+      // Disable the "delete" button.
+      $deleteButton.prop('disabled', true);
+    }
   }
+
 
   // "Digit" button click handler callback.
   $(".numbers-pad .digit").on("click", digitClickHandler);
@@ -57,44 +66,41 @@ $(document).ready(function() {
   });
 
   // Delete button click handler callback.
-  $deleteButton.on("click", deleteButtonkHandler);
+  $deleteButton.on("click", deleteButtonHandler);
 
   // Dummy server response.
   function serverResponse() {
 
-    // Dynamic icon.
-    var $icon = $('#dynamic-icon');
-
     // Displaying the loader.
-    $icon.append('<i class="fa fa-circle-o-notch fa-spin"></i>');
+    $dynamicIcon.append('<i class="fa fa-circle-o-notch fa-spin"></i>');
 
     setTimeout(function(){
-      // Clear inner content first.
-      $icon.html('');
+      // Delete icon.
+      $dynamicIcon.html('');
 
       // Reset all buttons.
       $('button').each(function() {
         $(this).prop('disabled', false);
       });
 
-      // In case of success.
+      // success.
       if ($('.code .pin').text() === validPinCode) {
-        $icon.append('<i class="fa fa-check -success"></i>');
+        $dynamicIcon.append('<i class="fa fa-check -success"></i>');
+
+        // Disable all buttons.
+        $('button').each(function() {
+          $(this).prop('disabled', true);
+        });
       }
-      // In case of error.
+      // Error.
       else {
-        $icon.append('<i class="fa fa-times -error"></i>');
+        $dynamicIcon.append('<i class="fa fa-exclamation-triangle -error"></i>');
       }
     }, 300)
-
-    // Disable all digits buttons it it's the 4th digit that was clicked.
-    $('button').each(function() {
-      $(this).prop('disabled', true);
-    });
   }
 
   // Toggle button activity
-  function toggoleButtonActivity(button) {
+  function toggleButtonActivity(button) {
     // Add active class
     $(button).addClass('-active');
 
