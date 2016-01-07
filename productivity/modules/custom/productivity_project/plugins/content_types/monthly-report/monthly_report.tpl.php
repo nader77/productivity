eee8bcff6bc02a432c1c9c676eb6214eeed4cfb3
Nir Galon <nirgn975@gmail.com>
2016-01-07 08:16:32 +0200

    Rearrange plugin code.

--- productivity/modules/custom/productivity_project/plugins/content_types/monthly-report/monthly_report.tpl.php
+++ productivity/modules/custom/productivity_project/plugins/content_types/monthly-report/monthly_report.tpl.php
@@ -1,46 +1,9 @@
-<?php
-
-/**
- * Plugin definition.
- */
-$plugin = array(
-  'title' => t('Monthly Report'),
-  'description' => t('Monthly Report.'),
-  'category' => t('huji'),
-  'hook theme' => 'productivity_monthly_report_content_type_theme',
-  'required context' => new ctools_context_required(t('Node'), 'node'),
-);
-
-/**
- * Render callback.
- */
-function productivity_monthly_report_content_type_render($subtype, $conf, $args, $context) {
-  $block = new stdClass();
-  $type = '';
-
-  $block->module = 'productivity';
-  $block->title = '';
-  $block->content = theme('productivity_monthly_report', array(
-      'tab_header' => productivity_monthly_report_info_render_tab_header(''),
-      'department_tabs' => '',
-      'entity_id' => '',
-    )
-  );
-  drupal_add_js(array('menu' => $type), 'setting');
-  return $block;
-}
+<div class="col-lg-4 col-md-6 col-sm-6">
+  <div class="main-box clearfix project-box emerald-box">
+    <div class="main-box-body clearfix">
+      <h1>Monthly Report</h1>
 
 
-
-/**
- * Delegated hook_theme().
- */
-function productivity_monthly_report_content_type_theme(&$theme, $plugin) {
-  $theme['productivity_monthly_report'] = array(
-    'variables' => array(
-      'vars' => NULL,
-    ),
-    'path' => $plugin['path'],
-    'template' => 'monthly_report',
-  );
-}
+    </div>
+  </div>
+</div>
