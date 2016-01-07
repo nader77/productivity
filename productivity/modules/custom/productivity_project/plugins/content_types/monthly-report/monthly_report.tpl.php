2af13865e315ec11fe69ed72a80db6d1cc74dbf5
Nir Galon <nirgn975@gmail.com>
2016-01-07 15:25:18 +0200

    Add github issues tables by type

--- productivity/modules/custom/productivity_project/plugins/content_types/monthly-report/monthly_report.tpl.php
+++ productivity/modules/custom/productivity_project/plugins/content_types/monthly-report/monthly_report.tpl.php
@@ -1,8 +1,11 @@
 <div class="col-lg-4 col-md-6 col-sm-6">
   <div class="main-box clearfix project-box emerald-box">
     <div class="main-box-body clearfix">
-      <h1>Monthly Report</h1>
+      <h1>Monthly Report - <?php print $project_title; ?></h1>
 
+      <?php foreach($tables as $table): ?>
+        <?php print $table; ?>
+      <?php endforeach; ?>
 
     </div>
   </div>
