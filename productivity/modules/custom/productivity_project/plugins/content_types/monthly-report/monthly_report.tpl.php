a3e6a88e041e57e6bea11dbaa1eac8ba07fdcd9f
Nir Galon <nirgn975@gmail.com>
2016-01-07 16:21:54 +0200

    Separate to functions.

--- productivity/modules/custom/productivity_project/plugins/content_types/monthly-report/monthly_report.tpl.php
+++ productivity/modules/custom/productivity_project/plugins/content_types/monthly-report/monthly_report.tpl.php
@@ -2,11 +2,10 @@
   <div class="main-box clearfix project-box emerald-box">
     <div class="main-box-body clearfix">
       <h1>Monthly Report - <?php print $project_title; ?></h1>
-
-      <?php foreach($tables as $table): ?>
+      <?php foreach($tables as $index => $table): ?>
+        <h2><?php print strtoupper($table_titles[$index]); ?></h2>
         <?php print $table; ?>
       <?php endforeach; ?>
-
     </div>
   </div>
 </div>
