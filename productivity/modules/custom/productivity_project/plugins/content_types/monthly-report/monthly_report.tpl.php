25a73993cb6b54f01e35d8e9d5dc12e542e35a57
Nir Galon <nirgn975@gmail.com>
2016-01-10 13:04:20 +0200

    Add total amount for all tables, and get the currency of the scope.

--- productivity/modules/custom/productivity_project/plugins/content_types/monthly-report/monthly_report.tpl.php
+++ productivity/modules/custom/productivity_project/plugins/content_types/monthly-report/monthly_report.tpl.php
@@ -1,12 +1,15 @@
 <div class="col-sm-12">
   <div class="main-box clearfix project-box emerald-box">
     <div class="main-box-body clearfix">
-      <h1>Monthly Report - <?php print $project_title; ?></h1>
+      <h1>Monthly Report<small> - <?php print $project_title; ?></small></h1>
+      <?php $total_tables_amount = 0; ?>
       <?php foreach($tables as $index => $table): ?>
         <h2><?php print strtoupper($table_titles[$index]); ?></h2>
         <?php print $table; ?>
-        <span class="pull-right">Total: <?php print $total_per_types[$table_titles[$index]]; ?></span>
+          <span class="pull-right">Total: <?php print $total_per_types[$table_titles[$index]] . ' ' . $total_currency_per_types[$table_titles[$index]]; ?></span>
+          <?php $total_tables_amount += $total_per_types[$table_titles[$index]] ?>
       <?php endforeach; ?>
+      <div class="col-sm-offset-9 col-sm-3">TOTAL: <?php print $total_tables_amount . ' ' . $total_currency_per_types[$table_titles[$index]]; ?></div>
     </div>
   </div>
 </div>
