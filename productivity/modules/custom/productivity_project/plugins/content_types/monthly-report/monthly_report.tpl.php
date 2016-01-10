9fb88a57db9c728962a370061359622e0b7f9775
Nir Galon <nirgn975@gmail.com>
2016-01-10 08:35:42 +0200

    Refactoring

--- productivity/modules/custom/productivity_project/plugins/content_types/monthly-report/monthly_report.tpl.php
+++ productivity/modules/custom/productivity_project/plugins/content_types/monthly-report/monthly_report.tpl.php
@@ -1,10 +1,11 @@
-<div class="col-lg-4 col-md-6 col-sm-6">
+<div class="col-sm-12">
   <div class="main-box clearfix project-box emerald-box">
     <div class="main-box-body clearfix">
       <h1>Monthly Report - <?php print $project_title; ?></h1>
       <?php foreach($tables as $index => $table): ?>
         <h2><?php print strtoupper($table_titles[$index]); ?></h2>
         <?php print $table; ?>
+        <span class="pull-right">Total: <?php print $total_types[$table_titles[$index]]; ?></span>
       <?php endforeach; ?>
     </div>
   </div>
