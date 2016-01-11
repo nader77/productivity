826978eeb9679efa216b834424335264874365f6
Nir Galon <nirgn975@gmail.com>
2016-01-11 16:30:41 +0200

    Finish pdf style.

--- productivity/modules/custom/productivity_project/plugins/content_types/monthly-report/monthly_report_pdf.tpl.php
+++ productivity/modules/custom/productivity_project/plugins/content_types/monthly-report/monthly_report_pdf.tpl.php
@@ -6,19 +6,28 @@
     }
     body {
       font-family: "Open Sans", sans-serif;
+      font-size: 0.85em;
     }
     .gizra-logo {
-      display: inline;
+      /*display: inline;*/
+      position: absolute;
       font-family: Abril Fatface;
-      font-size: 3.5em;
+      font-size: 4.5em;
       color: #e27058;
-      float: right;
+      margin-left: 350px;
+      margin-top: -30px;
     }
     h2 {
       display: inline;
     }
     h3.table-header {
-      margin-top: 1%;
+      margin-top: 25px;
+    }
+    small {
+      padding-left: 8px;
+      font-size: 90%;
+      color: #777;
+      font-weight: normal;
     }
     table {
       width: 100%;
@@ -36,10 +45,16 @@
     .table-striped > tbody > tr:nth-child(odd) > td, .table-striped > tbody > tr:nth-child(odd) > th {
       background-color: #f9f9f9;
     }
+    .table thead > tr > th {
+      text-align: left;
+    }
     .table tbody > tr > td {
-      vertical-align: middle;
+      /vertical-align: middle;
       border-top: 1px solid #e7ebee;
-      padding: 12px 8px;
+      padding: 12px 0;
+    }
+    .table tbody > tr:last-of-type() {
+      margin-bottom: 5%;
     }
     .col-sm-12 {
       padding: 0 8px;
@@ -48,28 +63,34 @@
       min-height: 1px;
     }
     .pull-right {
-      display: inline;
-      float: right;
+      position: absolute;
+      right: 0;
+    }
+    .total-pull-right {
+      margin-top: 20px;
+      position: absolute;
+      right: 0;
     }
   </style>
 </head>
 <body lang=EN-US>
 <div class="section">
-  <h2>Monthly Report<small> - <?php print $project_title; ?></small></h2>
-  <h1 class="gizra-logo">gizra</h1>
-
+  <div class="col-sm-12">
+    <h2>Monthly Report<small> - <?php print $project_title; ?></small></h2>
+    <h1 class="gizra-logo">gizra</h1>
+  </div>
   <!-- BEGIN Tables -->
   <?php $total_tables_amount = 0; ?>
   <?php foreach($tables as $index => $table): ?>
-    <h3 class="table-header"><?php print strtoupper($table_titles[$index]); ?></h3>
-    <?php print $table; ?>
     <div class="col-sm-12">
+      <h3 class="table-header"><?php print strtoupper($table_titles[$index]); ?></h3>
+      <?php print $table; ?>
       <span class="pull-right">Total: <?php print $total_per_types[$table_titles[$index]] . ' ' . $total_currency_per_types[$table_titles[$index]]; ?></span>
     </div>
     <?php $total_tables_amount += $total_per_types[$table_titles[$index]] ?>
   <?php endforeach; ?>
   <div class="col-sm-12">
-    <span class="pull-right">TOTAL: <?php print $total_tables_amount . ' ' . $total_currency_per_types[$table_titles[$index]]; ?></span>
+    <span class="total-pull-right">TOTAL: <?php print $total_tables_amount . ' ' . $total_currency_per_types[$table_titles[$index]]; ?></span>
   </div>
   <!-- END Tables -->
 
