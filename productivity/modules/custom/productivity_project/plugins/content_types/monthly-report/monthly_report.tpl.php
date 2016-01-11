bdbc925b2613593127ee22e1a7231487be56ab1b
Nir Galon <nirgn975@gmail.com>
2016-01-11 13:27:31 +0200

    Add Excel button and style the project select.

--- productivity/modules/custom/productivity_project/plugins/content_types/monthly-report/monthly_report.tpl.php
+++ productivity/modules/custom/productivity_project/plugins/content_types/monthly-report/monthly_report.tpl.php
@@ -34,6 +34,7 @@
         <?php $total_tables_amount += $total_per_types[$table_titles[$index]] ?>
       <?php endforeach; ?>
       <div class="col-sm-12 well">
+        <button type="button" class="btn btn-default">Download XLS</button>
         <span class="col-sm-2 pull-right">TOTAL: <?php print $total_tables_amount . ' ' . $total_currency_per_types[$table_titles[$index]]; ?></span>
       </div>
       <!-- END Tables -->
