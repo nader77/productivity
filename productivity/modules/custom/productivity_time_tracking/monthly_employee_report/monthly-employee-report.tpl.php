a12e58552f807c315b543ea589195a11c689e263
Itamar Shapira Bar-Lev <itamar@gizra.com>
2016-03-28 16:42:00 +0300

    WIP: Monthly employee report

---
+++ productivity/modules/custom/productivity_time_tracking/monthly_employee_report/monthly-employee-report.tpl.php
@@ -0,0 +1,39 @@
+<div class="row">
+  <div class="main-box clearfix project-box emerald-box col-sm-12">
+    <div class="main-box-body clearfix">
+      <p class="show-only-on-print"><?php print $date; ?></p>
+      <div id="header" class="col-sm-12">
+        <h1 id="project-title"><?php print t('Monthly Report'); ?><small><?php print ' - ' . $account . ' - ' . $project_title; ?></small></h1>
+        <h2 id="gizra-logo">gizra</h2>
+      </div>
+      <div id="search-filter" class="row">
+        <div class="col-sm-12 ">
+          <div class="col-sm-4"><p><?php print t('Project:'); ?> </p></div>
+          <div class="col-sm-4"><p><?php print t('Date:'); ?> </p></div>
+        </div>
+        <div class="col-sm-12">
+          <div class="col-sm-4">
+            <select id="project_filter" class="form-control">
+              <?php foreach ($employees as $uid => $account): ?>
+                <option value="<?php print $uid;?>"<?php print ($uid == $current_uid) ? 'selected' : ''; ?>><?php print $account->name; ?></option>
+              <?php endforeach; ?>
+            </select>
+          </div>
+          <div class="col-sm-4">
+              <input type="text" id="month" name="month" class="monthPicker form-control" />
+              <span class="add-on"><i class="icon-th"></i></span>
+          </div>
+          <div class="col-sm-4">
+            <button class="btn btn-primary apply" type="button">Apply</button>
+            <button class="btn btn-primary anytime" type="button">Any time</button>
+            <button class="btn btn-primary year" type="button" data-toggle="popover" title="Can't be done" data-content="Please select a year first">All Year</button>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm-12"><?php print $message; ?></div>
+      </div>
+     <?php print $table; ?>
+    </div>
+  </div>
+</div>
---
+++ productivity/modules/custom/productivity_time_tracking/monthly_employee_report/monthly_employee_report.js
@@ -0,0 +1,124 @@
+'use strict';
+
+(function ($) {
+
+
+  var month = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
+
+  /**
+   * Create new URL from project id and date.
+   *
+   * @param base_url
+   *  The base URL of the page.
+   *
+   * @returns {string}
+   *  New URL string.
+   */
+  function create_new_url(base_url, all, year) {
+    var project_id = $('#project_filter').val();
+    var val = $(".monthPicker").val();
+    var res = val.split(",");
+
+    // Full year link
+    if (year) {
+      if (res[1].trim() == 'all') {
+        return false;
+      }
+      return base_url + "/monthly-report/" + project_id + "/" + res[1].trim() + "/all";
+    }
+
+    // All time link.
+    if (all) {
+      return base_url + "/monthly-report/" + project_id + "/all/all";
+    }
+
+    // Specific month link
+    return base_url + "/monthly-report/" + project_id + "/" + res[1].trim() + "/" + get_month_num(res[0].trim());
+  }
+
+  /**
+   * Convert num to month.
+   */
+  function get_month_name(month_num) {
+    if (month_num == 'all') {
+      return month_num;
+    }
+    return month[month_num - 1];
+  }
+
+  /**
+   * Convert month to num.
+   */
+  function get_month_num(month_name) {
+    var i;
+    for (i = 0; i < 12; i++) {
+      if (month[i] == month_name) {
+        return i+1;
+      }
+    }
+    return 'all';
+  }
+
+  /**
+   * Set the current year and month on date input.
+   */
+  function set_date_input(settings) {
+    // get the current month and year.
+    var input_date = get_month_name(settings['monthly_report']['month']) + ', ' +  settings['monthly_report']['year'];
+    $('.monthPicker').attr('value', input_date);
+  }
+
+  Drupal.behaviors.monthlyReports = {
+    attach: function (context, settings) {
+      set_date_input(settings);
+      $('#project_filter').select2();
+      $(".btn.year").popover({delay: { "show": 500, "hide": 100 }});
+
+      // Apply filter button handler.
+      $('.apply').click(function() {
+        window.location.href = create_new_url(settings['monthly_report']['base_url'], false, false);
+      });
+
+      $('.anytime').click(function() {
+        window.location.href = create_new_url(settings['monthly_report']['base_url'], true, false);
+      });
+
+      $('.year').click(function() {
+        var link = create_new_url(settings['monthly_report']['base_url'], true, true);
+        if (!link) {
+          // Initializes popovers for an element collection.
+          //$().popover(options);
+          $(".btn.year").popover('show');
+        }
+        else {
+          window.location.href = link;
+        }
+      });
+
+      $('input[name=month]').datepicker( {
+        format: "MM, yyyy",
+        minViewMode: 1,
+        autoclose: true,
+        startDate: "2/2016",
+        startView: 1,
+        todayBtn: "linked",
+        keyboardNavigation: false,
+        forceParse: false
+      } );
+    }
+  };
+})(jQuery);
+
