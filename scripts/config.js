b33def47118d01cc945c4302c9778d3971256d18
nader77 <nader@gizra.com>
2016-04-04 16:10:26 +0200

    Built client from commit ac97fb9 on branch pantheon-deployment-fixes

--- scripts/config.js
+++ scripts/config.js
@@ -1,5 +1,5 @@
 angular.module('config', [])
 
-.constant('Config', {backend:'http://live-productivity.pantheon.io',debugUiRouter:false,githubClientId:'9169e930cd825924839a',calendar:false,local:false})
+.constant('Config', {backend:'http://live-productivity.pantheon.io',debugUiRouter:false,githubClientId:'9169e930cd825924839a',calendar:false,local:true})
 
 ;
