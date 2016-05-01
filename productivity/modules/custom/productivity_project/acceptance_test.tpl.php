06423ed675734bed13ff461fadd439f253da2ee4
Brice Lenfant <brice@gizra.com>
2016-05-02 01:05:04 +0300

    New acceptance test page.

---
+++ productivity/modules/custom/productivity_project/acceptance_test.tpl.php
@@ -0,0 +1,44 @@
+<form id="acceptance" class="form-horizontal">
+  <fieldset>
+    <table class="table table-bordered table-striped table-hover table-condensed table-responsive">
+      <thead>
+      <tr>
+        <th>
+          Types
+        </th>
+        <th>
+          Status
+        </th>
+        <th>
+          Name of person approving
+        </th>
+      </tr>
+      </thead>
+      <tbody>
+        <?php print $rows; ?>
+      </tbody>
+    </table>
+    <span id="submit_result"></span>
+  </fieldset>
+</form>
+
+<script>
+  jQuery("#acceptance select").change(function(event){
+    var data = {};
+    jQuery("#acceptance .form-data, #acceptance select").each(function() {
+      data[jQuery(this).attr('id')] = jQuery(this).attr('value');
+    });
+    jQuery('#submit_result').text('Saving...').show();
+    jQuery.ajax({
+      type: "POST",
+      url: Drupal.settings.submitAcceptanceURL,
+      data: data
+      })
+      .done(function() {
+        jQuery('#submit_result').text('Saving...').fadeOut();
+      })
+      .fail(function() {
+        jQuery('#submit_result').text('failed to Saved').fadeOut();
+      });
+  });
+</script>
